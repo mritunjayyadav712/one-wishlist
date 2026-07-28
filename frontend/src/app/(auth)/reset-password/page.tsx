381f5d3ce51d0404733569ffd2c53e79892e726f
Mrityunjay <mritunjayyadav712@gmail.com>
2026-07-29 05:19:51 +0530

    vercel dploy fix 2nd try

--- frontend/src/app/(auth)/reset-password/page.tsx
+++ frontend/src/app/(auth)/reset-password/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import ResetPasswordClient from "./reset-password-client";
+import ResetPasswordClient from "./ResetPasswordClient";
 
 export default function ResetPasswordPage() {
   return (
